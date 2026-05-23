3d5113610abe5b6d8c4682ae53ba1e3598360f43
thelazybastard <256585194+thelazybastard@users.noreply.github.com>
2026-05-23 14:20:05 +0700

    added args check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// 2 args. args 0 is binary so exclude that. args1 wil be either install or check. args2 is either empty or
+// the commit message
+// 
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args[1].as_str() {
+        "install" => install(),
+        "check" => check(),
+        _ => println!("Unknown command. use 'porter' install or 'porter check'")
+    }
+}
+
+fn install() {
+    println!("Install");
+}
+
+fn check() {
+    println!("Check");
 }
